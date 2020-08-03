10173b7829912a49eaa881e4d841f5e6b66063ad
MazeArt <fcoarturo.marshall@gmail.com>
2020-08-03 09:59:22 -0400

    added branch adding_hover

---
+++ readme.txt
@@ -0,0 +1,12 @@
+Tasks
+[ ] define table properties as cell width
+[ ] merge verb cells 
+[ ] make top row fixed
+
+Done
+[x] create a base table 
+[x] make some of the cells as inputs
+[x] create the base table constructor logic for 1 verb
+[x] contrast input with correct answer and color the cell
+[x] hover functionalty
+[x] create mulitple verbs dynamically w/ datascrture
--- script.js
+++ script.js
@@ -1,109 +1,83 @@
+/*
+
+
+*/
 
 var table = document.createElement('table');
 table.classList.add('table');
+//table.id="MyTable" here goes the CSS tbale design
 
 var thead = document.createElement('thead');
 var headRow = document.createElement('tr');
 
 const temposVerbales=['Presente','Preterito Imperfeito'];
 const pessoaGramatical=['eu', 'você / ele / ela','nós','vocês / eles / elas'];
-const verbos=['ser'];
 var columnNames = ["Verbo", "Pers", 'PRESENTE',	'PRET PERFEITO','PRET IMPERFEITO',	'FUTURO DO PRESENTE',	'FUTURO DO PRETERITO'];
 var tempCodes = ['prs','ptp','pti','fps','fpt']
 var pessoaCodes =['1s','3s','1p','3p']
- 
+
 
 for (var i = 0; i < columnNames.length; i++) {
   var th = document.createElement('th');
   th.appendChild(document.createTextNode(columnNames[i]));
   headRow.appendChild(th);
 }
+num_verbos=verbosB.length
 
-thead.appendChild(headRow);
-
-var tbody = document.createElement('tbody');
+let verbos=[];
+for(var i=0;i<verbosB.length;i++){
+  var get_verbo_from_list=verbosB[i][Object.keys(verbosB[i])[0]];
+  verbos.push(get_verbo_from_list)
+console.log(verbos)
+}
 
 var numRows=pessoaGramatical.length*verbos.length
 
-for (var i = 0; i < numRows; i++) {
+thead.appendChild(headRow);
+var tbody = document.createElement('tbody');
+for (var v = 0; v < verbos.length; v++) {
+  for (var i = 0; i < pessoaGramatical.length; i++) {
 
-  var tr = document.createElement('tr');
+    var tr = document.createElement('tr');
 
-  for (var j = 0; j < columnNames.length; j++) {
-    var td = document.createElement('td');
+    for (var j = 0; j < columnNames.length; j++) {
+      var td = document.createElement('td');
 
-    if (j > 1) { //tempos cols
-      td.classList.add("CellWithComment");
-      var input = document.createElement('input');
-      var span = document.createElement('span')
-      input.type = "text";
-      span.classList.add("CellComment0")
-      td.appendChild(input);
-      td.appendChild(span);
+      if (j > 1) { //tempos cols
+        td.classList.add("CellWithComment");
+        var input = document.createElement('input');
+        var span = document.createElement('span')
+        input.type = "text";
+        span.classList.add("CellComment0")
+        td.appendChild(input);
+        td.appendChild(span);
+        tr.appendChild(td);
+        input.id=verbos[v]+"_"+tempCodes[j-2]+'_'+pessoaCodes[i];
+        input.size='10';
+        span.id=verbos[v]+"_"+tempCodes[j-2]+'_'+pessoaCodes[i]+'_a';
+        //input.class="search";
+        //input.onkeypress='process(event, this)';
+        input.onkeypress= new Function("return process(event, this)");
+        continue;
+      }
+      if (j == 1) {
+        td.appendChild(document.createTextNode(pessoaGramatical[i]));
+      }
+      if (j == 0) {
+        td.appendChild(document.createTextNode(verbos[v]));
+      }
+    
       tr.appendChild(td);
-      input.id=tempCodes[j-2]+'_'+pessoaCodes[i];
-      input.size='10';
-      span.id=tempCodes[j-2]+'_'+pessoaCodes[i]+'_a';
-      //input.class="search";
-      //input.onkeypress='process(event, this)';
-      input.onkeypress= new Function("return process(event, this)");
-      continue;
     }
-    if (j == 1) {
-      td.appendChild(document.createTextNode(pessoaGramatical[i]));
-    }
-    if (j == 0) {
-      td.appendChild(document.createTextNode("ser"));
-    }
-   
-    tr.appendChild(td);
-  }
 
-  tbody.appendChild(tr);
-}
+    tbody.appendChild(tr);
+  }
+} 
 
 table.appendChild(thead);
 table.appendChild(tbody);
 document.body.appendChild(table);
 
-
-
-const ser=['']
-
-let verbosA = [{verbo: 'ser',
-  prs_1s: 'sou',
-  prs_3s: 'é',
-  prs_1p: 'somos',
-  prs_3p: 'são',
-  ptp_1s: 'fui',
-  ptp_3s: 'foi',
-  ptp_1p: 'fomos',
-  ptp_3p: 'foram',
-  pti_1s: 'era',
-  pti_3s: 'era',
-  pti_1p: 'éramos',
-  pti_3p: 'eram',
-  fps_1s: 'serei',
-  fps_3s: 'será',
-  fps_1p: 'seremos',
-  fps_3p: 'serão',
-  fpt_1s: 'seria',
-  fpt_3s: 'seria',
-  fpt_1p: 'seríamos',
-  fpt_3p: 'seriam'
-}];
-
-
-let customers = [{
-  name: 'ABC Inc',
-  credit: 100
-}, {
-  name: 'ACME Corp',
-  credit: 200
-}, {
-  name: 'IoT AG',
-  credit: 300
-}];
 
 function search(ele) {
   if(event.key === 'Enter') {
@@ -117,8 +91,16 @@ function process(e,obj) {
   if (code == 13) { //Enter keycode
 
     // contrast input vs correct
-    //add verbo to obj.id search
-    var correctAns=pppp[obj.id] //e.g. obj.id is 'prs_1s'
+    //split verbo from obj.id 
+    var split_str=obj.id.split("_");
+    console.log(split_str);
+    verbo_str=split_str[0];
+    temp_pessoa=split_str[1]+"_"+split_str[2];
+    console.log(verbo_str);
+
+    var pppp= verbosB.find(c => c.verbo == verbo_str)
+
+    var correctAns=pppp[temp_pessoa] //e.g. temp_pessoa is 'prs_1s'
     var typedAns=obj.value
     if(typedAns==correctAns){
       obj.style.backgroundColor='#90EE90';
@@ -134,9 +116,11 @@ function process(e,obj) {
      document.getElementById(obj.id+"_a").classList.remove("CellComment0")
      document.getElementById(obj.id+"_a").classList.add("CellComment1") // how to change "hover" property here?
     }
-    $(this).next().find('input[type=text]').focus()
+   // $(this).next().find('input[type=text]').focus() //not working focus
   }
 }
 
-var pppp= verbosA.find(c => c.verbo =='ser')
 //pppp.pr1p = 'sou'
+
+//TODO https://stackoverflow.com/questions/13845003/tooltips-for-cells-in-html-table-no-javascript
+//implementation of table style
